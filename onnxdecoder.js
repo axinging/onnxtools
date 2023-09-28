7aeaa26267da837211baa4572a39a987e960c9d2
Xu Xing <xing.xu@intel.com>
2023-09-28 18:35:42 +0800

    First version

--- onnxdecoder.js
+++ onnxdecoder.js
@@ -2,7 +2,7 @@ import * as onnxModule from './onnx.js';
 import {tensorDataTypeStringToEnum } from "./onnxdecoder_util.js";
 const onnx = onnxModule.onnx;
 
-export function createOnnxModel(test) {
+export async  function createOnnxModel(test) {
   const opsetImport = onnx.OperatorSetIdProto.create(test.opset);
   const operator = test.operator;
   const attribute = (test.attributes || []).map(attr => {
@@ -142,6 +142,50 @@ export function createOnnxModel(test) {
     const backendHint = test.backend;
     const loadedData = onnx.ModelProto.encode(model).finish();
 
-  
-  return model;
+    const session = await ort.InferenceSession.create(
+      loadedData, {executionProviders: [backendHint], ...{}});
+  return  session;
+}
+
+export async function runProtoOpTestcase(
+  session, testCase){
+const feeds = {};//: Record<string, ort.Tensor> = {};
+const fetches = [];//: string[] = [];
+testCase.inputs.forEach((input, i) => {
+  if (input.data) {
+    let data = input.data;
+    if (input.type === 'uint64') {
+      data = BigUint64Array.from(input.data.map(BigInt));
+    } else if (input.type === 'int64') {
+      data = BigInt64Array.from(input.data.map(BigInt));
+    }
+    feeds[`input_${i}`] = new ort.Tensor(input.type, data, input.dims);
+  }
+});
+
+const outputs = [];//: ort.Tensor[] = [];
+const expectedOutputNames = [];// string[] = [];
+testCase.outputs.forEach((output, i) => {
+  if (output.data) {
+    let data = output.data;
+    if (output.type === 'uint64') {
+      data = BigUint64Array.from(output.data.map(BigInt));
+    } else if (output.type === 'int64') {
+      data = BigInt64Array.from(output.data.map(BigInt));
+    }
+    outputs.push(new ort.Tensor(output.type, data, output.dims));
+    expectedOutputNames.push(`output_${i}`);
+    fetches.push(`output_${i}`);
+  }
+});
+
+const results = await session.run(feeds, fetches);
+return results;
+
+// const actualOutputNames = Object.getOwnPropertyNames(results);
+// expect(actualOutputNames.length).to.equal(expectedOutputNames.length);
+// expect(actualOutputNames).to.have.members(expectedOutputNames);
+// 
+// const actualOutputs = actualOutputNames.map(name => results[name]);
+// validator.checkApiTensorResult(actualOutputs, outputs);
 }
